a67349006c240e479e7bd6c4df8fb1a26397e46d
arhansuba <subasiarhan3@gmail.com>
2024-08-19 00:01:36 +0300

    commit 4

--- contracts/real-estate-marketplace/Property.sol
+++ contracts/real-estate-marketplace/Property.sol
@@ -3,71 +3,14 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Property is ERC721Enumerable, Ownable {
-    struct PropertyDetails {
-        uint256 propertyId;
-        string propertyName;
-        string location;
-        uint256 value;
-        bool isBasket;
-        uint256[] basketProperties; // Array of property IDs if this is a basket
-    }
+contract Property is ERC721Enumerable, Ownable, Pausable {
+    using SafeERC20 for IERC20;
 
-    mapping(uint256 => PropertyDetails) public properties;
-
-    constructor() ERC721("RealEstateProperty", "PROP") {}
-
-    function createProperty(
-        string memory name,
-        string memory location,
-        uint256 value,
-        bool isBasket,
-        uint256[] memory basketProperties
-    ) external onlyOwner returns (uint256) {
-        uint256 propertyId = totalSupply() + 1;
-        _mint(msg.sender, propertyId);
-
-        properties[propertyId] = PropertyDetails({
-            propertyId: propertyId,
-            propertyName: name,
-            location: location,
-            value: value,
-            isBasket: isBasket,
-            basketProperties: basketProperties
-        });
-
-        return propertyId;
-    }
-
-    function getPropertyDetails(uint256 propertyId)
-        external
-        view
-        returns (PropertyDetails memory)
-    {
-        return properties[propertyId];
-    }
-
-    // Define events
-    event PropertyUpdated(
-        uint256 indexed propertyId,
-        address indexed owner,
-        string newDescription,
-        uint256 newPrice
-    );
-    event OwnershipTransferred(
-        uint256 indexed propertyId,
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-
-    // Define state variables
+    // Struct for Property Details
     struct PropertyDetails {
         address owner;
         string description;
@@ -76,22 +19,27 @@ contract Property is ERC721Enumerable, Ownable {
         address paymentToken;
     }
 
+    // State variables
     mapping(uint256 => PropertyDetails) public properties;
     uint256 public nextPropertyId;
 
+    // Events
+    event PropertyCreated(uint256 indexed propertyId, address indexed owner, string description, uint256 price);
+    event PropertyUpdated(uint256 indexed propertyId, address indexed owner, string newDescription, uint256 newPrice);
+    event OwnershipTransferred(uint256 indexed propertyId, address indexed previousOwner, address indexed newOwner);
+
     // Constructor
-    constructor() {}
+    constructor() ERC721("RealEstateProperty", "PROP")Ownable(msg.sender) Pausable() {}
 
     // Function to create a new property
-    function createProperty(
-        string calldata description,
-        uint256 price,
-        address paymentToken
-    ) external whenNotPaused onlyOwner {
+    function createProperty(string calldata description, uint256 price, address paymentToken) external whenNotPaused onlyOwner {
         require(price > 0, "Price must be greater than 0");
         require(paymentToken != address(0), "Invalid payment token address");
 
-        properties[nextPropertyId] = PropertyDetails({
+        uint256 propertyId = nextPropertyId;
+        _mint(msg.sender, propertyId);
+
+        properties[propertyId] = PropertyDetails({
             owner: msg.sender,
             description: description,
             price: price,
@@ -99,21 +47,14 @@ contract Property is ERC721Enumerable, Ownable {
             paymentToken: paymentToken
         });
 
-        emit PropertyUpdated(nextPropertyId, msg.sender, description, price);
+        emit PropertyCreated(propertyId, msg.sender, description, price);
         nextPropertyId++;
     }
 
     // Function to update property details
-    function updateProperty(
-        uint256 propertyId,
-        string calldata newDescription,
-        uint256 newPrice
-    ) external whenNotPaused {
+    function updateProperty(uint256 propertyId, string calldata newDescription, uint256 newPrice) external whenNotPaused {
         PropertyDetails storage property = properties[propertyId];
-        require(
-            property.owner == msg.sender,
-            "Only the owner can update property details"
-        );
+        require(property.owner == msg.sender, "Only the owner can update property details");
         require(newPrice > 0, "Price must be greater than 0");
 
         property.description = newDescription;
@@ -123,33 +64,21 @@ contract Property is ERC721Enumerable, Ownable {
     }
 
     // Function to set property for sale
-    function setPropertyForSale(uint256 propertyId, bool forSale)
-        external
-        whenNotPaused
-    {
+    function setPropertyForSale(uint256 propertyId, bool forSale) external whenNotPaused {
         PropertyDetails storage property = properties[propertyId];
-        require(
-            property.owner == msg.sender,
-            "Only the owner can set property for sale"
-        );
+        require(property.owner == msg.sender, "Only the owner can set property for sale");
 
         property.isForSale = forSale;
     }
 
     // Function to transfer ownership of the property
-    function transferOwnership(uint256 propertyId, address newOwner)
-        external
-        whenNotPaused
-    {
+    function transferOwnership(uint256 propertyId, address newOwner) external whenNotPaused {
         PropertyDetails storage property = properties[propertyId];
-        require(
-            property.owner == msg.sender,
-            "Only the owner can transfer ownership"
-        );
+        require(property.owner == msg.sender, "Only the owner can transfer ownership");
         require(newOwner != address(0), "Invalid new owner address");
 
         property.owner = newOwner;
-        property.isForSale = false; // Automatically remove from sale
+        property.isForSale = false;  // Automatically remove from sale
 
         emit OwnershipTransferred(propertyId, msg.sender, newOwner);
     }
@@ -179,10 +108,4 @@ contract Property is ERC721Enumerable, Ownable {
     function unpause() external onlyOwner {
         _unpause();
     }
-
-    // Placeholder for external integration (e.g., with external systems or protocols)
-    //function integrateWithExternalSystem(uint256 propertyId) external view returns (bool) {
-    // Example integration logic
-    //  return true;
-    // }
 }
--- contracts/real-estate-marketplace/Shares.sol
+++ contracts/real-estate-marketplace/Shares.sol
@@ -2,27 +2,37 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Shares is Ownable, Pausable {
+contract Shares is ERC20, Ownable, Pausable {
     using Math for uint256;
     using SafeERC20 for IERC20;
 
-struct ShareBasket {
+    struct ShareBasket {
         uint256 basketId;
         uint256[] propertyIds;
         uint256 totalShares;
     }
 
-    
-    mapping(uint256 => ShareBasket) public baskets;
-    mapping(uint256 => uint256) public basketShares; // Basket ID to total shares
+    struct PropertyShares {
+        uint256 totalShares;
+        uint256 availableShares;
+        mapping(address => uint256) shareholders;
+    }
 
-    constructor() ERC20("PropertyShare", "SHARE") {}
+    mapping(uint256 => ShareBasket) public baskets;
+    mapping(uint256 => uint256) public basketShares;
+    mapping(uint256 => PropertyShares) public propertyShares;
+    mapping(uint256 => address) public propertyOwners;
+
+    event SharesIssued(address indexed investor, uint256 propertyId, uint256 sharesIssued);
+    event SharesTransferred(address indexed from, address indexed to, uint256 propertyId, uint256 sharesTransferred);
+
+    constructor() ERC20("PropertyShare", "SHARE") Ownable(msg.sender) {
+    }
 
     function createBasket(uint256 basketId, uint256[] memory propertyIds, uint256 totalShares) external onlyOwner {
         baskets[basketId] = ShareBasket({
@@ -42,28 +52,8 @@ struct ShareBasket {
     function getBasketDetails(uint256 basketId) external view returns (ShareBasket memory) {
         return baskets[basketId];
     }
-}
-    // Define events
-    event SharesIssued(address indexed investor, uint256 propertyId, uint256 sharesIssued);
-    event SharesTransferred(address indexed from, address indexed to, uint256 propertyId, uint256 sharesTransferred);
 
-    // Define state variables
-    struct PropertyShares {
-        uint256 totalShares;
-        uint256 availableShares;
-        mapping(address => uint256) shareholders;
-    }
-
-     mapping(uint256 => PropertyShares) propertyShares;
-    mapping(uint256 => address)  propertyOwners; // Property owners mapping (for cross-checking ownership)
-
-    // Constructor
-    constructor() Ownable(msg.sender) {
-        _pause(); // Initialize the contract in a paused state
-    }
-
-    // Function to issue shares for a specific property
-    function issueShares(uint256 propertyId, address investor, uint256 shares) external onlyOwner whenNotPaused {
+    function issuePropertyShares(uint256 propertyId, address investor, uint256 shares) external onlyOwner whenNotPaused {
         require(shares > 0, "Shares must be greater than 0");
         require(propertyShares[propertyId].availableShares >= shares, "Not enough shares available");
         
@@ -75,7 +65,6 @@ struct ShareBasket {
         emit SharesIssued(investor, propertyId, shares);
     }
 
-    // Function to transfer shares between investors
     function transferShares(uint256 propertyId, address to, uint256 shares) external whenNotPaused {
         require(to != address(0), "Invalid recipient address");
         require(propertyShares[propertyId].shareholders[msg.sender] >= shares, "Not enough shares to transfer");
@@ -87,43 +76,35 @@ struct ShareBasket {
         emit SharesTransferred(msg.sender, to, propertyId, shares);
     }
 
-    // Function to get the number of shares held by an address
     function getShares(uint256 propertyId, address shareholder) external view returns (uint256) {
         return propertyShares[propertyId].shareholders[shareholder];
     }
 
-    // Function to get the total number of shares issued for a property
     function getTotalShares(uint256 propertyId) external view returns (uint256) {
         return propertyShares[propertyId].totalShares;
     }
 
-    // Function to get the available shares for a property
     function getAvailableShares(uint256 propertyId) external view returns (uint256) {
         return propertyShares[propertyId].availableShares;
     }
 
-    // Function to set the property owner address (useful for verification or integration)
     function setPropertyOwner(uint256 propertyId, address owner) external onlyOwner {
         require(owner != address(0), "Invalid owner address");
         propertyOwners[propertyId] = owner;
     }
 
-    // Function to get the property owner
     function getPropertyOwner(uint256 propertyId) external view returns (address) {
         return propertyOwners[propertyId];
     }
 
-    // Function to pause the contract in emergencies
     function pause() external onlyOwner {
         _pause();
     }
 
-    // Function to unpause the contract
     function unpause() external onlyOwner {
         _unpause();
     }
 
-    // Placeholder for external integration (e.g., with external systems or protocols)
     function integrateWithExternalSystem(uint256 /* propertyId */) external pure returns (bool) {
         // Example integration logic
         return true;
